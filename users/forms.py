1909230699afa2079f7021ffba1bd74026724944
mirafzal114 <mirafzaldevelop@gmail.com>
2023-07-06 15:24:21 +0500

    endi UserCreate formni ozimiz qolda UserRegister ga alashtidi va yasadik

---
+++ users/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+'''bu UserCreate formni qolda email qoshiib chiqan vationatimiz'''
+class UserRegister(UserCreationForm):
+    email = forms.EmailField(required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email','password1','password2']
+
--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+
+class Post(models.Model):
+    fname = models.CharField(max_length=100)
+    lname = models.CharField(max_length=30)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    data = models.DateTimeField(default=timezone.now)
