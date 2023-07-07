fe8b7b73de268a802b856b1a9b0ea9a14e19ab2d
mirafzal114 <mirafzaldevelop@gmail.com>
2023-07-07 19:35:30 +0500

    kop narsa qoshildi assoan delte update detail va boshqalar

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='blog-home'),
+    path('post/<int:pk>/', views.PostDetail.as_view(), name='post-detail'),
+    path('post/new/', views.CreatePost.as_view(), name='new-post'),
     path('about/', views.about, name='blog-about'),
+    path('post/<int:pk>/update', views.PostUpdate.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', views.PostDelete.as_view(), name='delete-post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,45 @@
 from django.shortcuts import render
 from users.models import Post
 from django.views.generic.list import ListView
-# #
-# def home(request):
-#     # postlar = Post.objects.all()
-#     return render(request, 'blog/home.html')
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
 
 class Home(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
+   # ordering =  bu saralaydi oziga qarab vaxtmi joy shunga oxshash
+
+class PostDetail(DetailView):
+    model = Post
+
+class CreatePost(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    # success_url = 'blog-home'
+
+class PostDelete(DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog-home')
 
 
 def about(request):
     return render(request, 'blog/about.html')
+
+
