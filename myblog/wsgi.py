b0f9a7deeba69309b0f54bf28a1377e1369c945b
mirafzalswe <mirafzaldevelop@gmail.com>
2025-01-09 17:26:40 +0500

    done

--- myblog/wsgi.py
+++ myblog/wsgi.py
@@ -8,12 +8,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
 
-# app = get_wsgi_application()
 application = get_wsgi_application()
-# api/wsgi.py
-
